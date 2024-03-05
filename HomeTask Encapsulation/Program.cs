584c580eab76e2a8559e749b12d4c700c7a19b7f
mamedovx999 <mamedovx999@code.edu.az>
2024-03-05 18:40:57 +0400

    Add project files.

---
+++ HomeTask Encapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HomeTask_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Memmedeli", "M1234MAQA435");
+            user.Age = 21;
+            Console.WriteLine(user.Username);
+            Console.WriteLine(user.Age);
+            Console.WriteLine(user.Password);
+
+
+        }
+    }
+}
---
+++ HomeTask Encapsulation/User.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_Encapsulation
+{
+    internal class User
+    {
+        public string Username { get; set; }
+        private string _password;
+        private int _age;
+        public int Age {
+            get
+            {               
+              return _age;      
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("menfi olmaz");
+                    return;
+                }  
+                _age = value;
+
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("bos olmaz");
+                    return;
+                }
+                if (value.Length < 8)
+                {
+                    Console.WriteLine("Uzunluğu 8-dən kiçik ola bilməz");
+                    return;
+                }
+                if (CheckCapitalLetter(value))
+                {
+                    Console.WriteLine("İçində ən az bir böyük hərf olmalıdır");
+                    return;
+                }
+                if (!CheckNumber(value))
+                {
+                    Console.WriteLine("İçində ən az bir rəqəm olmalıdır");
+                    return;
+
+                }
+                _password = value;
+
+
+
+            }
+        }
+        public User (string username, string password)
+        {
+           Username = username;
+           Password = password;
+        }
+        public User(string username, string password, int age)
+        {
+            Username = username;
+            Password = password;
+            Age = age;
+        }
+
+        private bool CheckCapitalLetter(string password)
+        {
+            foreach (char c in password)
+            {
+                if (!char.IsUpper(c) && char.IsLetter(c))
+                    return true;
+            }
+            return false;
+        }
+
+        private bool CheckNumber(string password)
+        {
+            foreach(char c in password)
+            {
+                if(!char.IsDigit(c))
+                    return true;
+            }
+            return false;
+        }
+
+    }
+}
